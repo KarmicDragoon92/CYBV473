c85b946ce0761cd57d645f313ee0e96f7021881f
KarmicDragoon92 <sksundstrom@gmail.com>
2023-01-28 21:03:30 -0500

    Update SundstromK_WK2-1_script.py

--- SundstromK_WK2-1_script.py
+++ SundstromK_WK2-1_script.py
@@ -4,29 +4,13 @@ Week Two Assignment - File Processing
 Kaleb Sundstrom, 2023-01-25
 '''
 
-'''
-Complete the script below to do the following:
-1) Add your name, date, assignment number to the top of this script
-2) Open the file redhat.txt 
-   a) Iterate through each line of the file
-   b) Split eachline into individual fields (hint str.split() method)
-   c) Examine each field of the resulting field list
-   d) If the word "worm" appears in the field then add the worm name to the set of worms
-   e) Once you have processed all the lines in the file
-      sort the set 
-      iterate through the set of worm names
-      print each unqiue worm name 
-3) Submit
-   NamingConvention: lastNameFirstInitial_Assignment_.ext
-   for example:  hosmerC_WK2-1_script.py
-                 hosmerC_WK2-2_screenshot.jpg
-   A) Screenshot of the results in WingIDE
-   B) Your Script
-'''
-
 import os
+from prettytable import PrettyTable
 
 uniqueWorms = {}
+uniqueWormsTable = PrettyTable()
+
+uniqueWormsTable.field_names = ['Worms Name', 'Number of Occurences']
 
 with open("redhat.txt", 'r') as logFile:
     for eachLine in logFile:
@@ -38,7 +22,11 @@ with open("redhat.txt", 'r') as logFile:
                     uniqueWorms[eachField] = 1
             if eachField in uniqueWorms:
                 uniqueWorms[eachField] += 1
-print(uniqueWorms)
+
+for k, v in uniqueWorms.items():
+    uniqueWormsTable.add_row([k, v])
+
+print(uniqueWormsTable)
                 
                 
             
