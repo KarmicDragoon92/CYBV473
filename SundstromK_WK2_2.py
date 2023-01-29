55a8a184c2c7868dd083e6a7b20ba5f59fe707c3
KarmicDragoon92 <sksundstrom@gmail.com>
2023-01-28 21:03:58 -0500

    Update SundstromK_WK2_2.py

--- SundstromK_WK2_2.py
+++ SundstromK_WK2_2.py
@@ -7,6 +7,7 @@ Kaleb Sundstrom, 2023-01-2023
 
 import os
 import hashlib
+from prettytable import PrettyTable
 
 def hash_file(FILE):
     with open(FILE, 'rb') as FILE_Hash:
@@ -16,7 +17,10 @@ def hash_file(FILE):
         hashed_file = hashObj.hexdigest()
         return hashed_file
 
-DIR = "./"
+hash_table = PrettyTable()
+hash_table.field_names = ['Hash Value', 'File Path']      
+
+DIR = input('Input file path for dir you would like to walk: ')
 
 fileList   = []
 fileHashes = {}
@@ -34,8 +38,8 @@ for root, dirs, files in os.walk(DIR):
         fileHashes[fileHash] = fullPath
 
 for keys, values in fileHashes.items():
-    print(f'{keys}: {values}')
+    hash_table.add_row([keys, values])
       
-
+print(hash_table)
         
         
